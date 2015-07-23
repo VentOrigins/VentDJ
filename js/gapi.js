7c42dcbae835c847f44321ee7c521eb7209c5f86
Randy Truong <rtruo001@ucr.edu>
2015-07-23 04:29:10 -0700

    Updated mobile scaling

--- js/gapi.js
+++ js/gapi.js
@@ -3,12 +3,3 @@
     Copyright © Vent Origins 
     By Adrian Mandee and Randy Truong
     ========================================================================== */
-
-function search() {
-	event.preventDefault();
-	console.log("Hello");
-	console.log($("#query-input").val());
-
-  // Displays tracks
-  overlayTracks("#splash-screen");
-}
---
+++ js/overlay.js
@@ -0,0 +1,23 @@
+/*  =============================================================================
+    
+    Copyright © Vent Origins 
+    By Adrian Mandee and Randy Truong
+    ========================================================================== */
+
+function overlayTracks(divToOverlay) {
+  console.log("Overlaying tracks");
+  $(divToOverlay).append("<div class='overlay'>" );
+  $(divToOverlay).append("</div>");
+}
+
+/*  =============================================================================
+    Called in: displayTracks.js and displayPlaylist.js
+    Ends the loading screen for the given div given by the arguments
+    @param      div     The div to remove the overlayn
+    
+    @return     none
+    ========================================================================== */
+function finishedOverlaying(divToOverlay) {
+  $(divToOverlay + " > div[class='overlay']").remove();
+  console.log("FINISHED REMOVING OVERLAY\n\n");
+}
